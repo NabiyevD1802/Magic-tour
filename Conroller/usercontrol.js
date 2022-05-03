39031230fab8ff139a9c77a57af73d4a1db94246
NabiyevD1802 <nabiyevdavlatbek1818@gmail.com>
2022-05-03 20:44:03 +0500

    sal

--- Conroller/usercontrol.js
+++ Conroller/usercontrol.js
@@ -1,33 +1,3 @@
-const fs = require('fs');
-const getusers = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: ' this message ishlamadi',
-  });
-};
-const getuser = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: ' this message ishlamadi',
-  });
-};
-const postUser = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: ' this message ishlamadi',
-  });
-};
+// const fs = require('fs');
 
-const patchUser = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: ' this message ishlamadi',
-  });
-};
-const deleteUser = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    message: ' this message ishlamadi',
-  });
-};
-module.exports = { getusers, getuser, postUser, patchUser, deleteUser };
+// module.exports = { getusers, getuser, postUser, patchUser, deleteUser };
